c2c5e945bbe96af64444db4aa23342f3e6b4073e
Imaad Uddin <imaaduddin@icloud.com>
2021-04-28 00:01:27 -0500

    coloring background

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import pygame
 
 WIDTH, HEIGHT = 900, 500
-
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("First Game!")
 
 def main():
 
@@ -11,6 +11,8 @@ def main():
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         run = False
+
+      WIN.fill((25, 56, 78))
   pygame.quit()
 
 if __name__ == "__main__":
